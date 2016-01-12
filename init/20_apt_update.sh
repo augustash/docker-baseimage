838aa62b4b8503994b07a6c64ed96c20c25d5d13
Pete McWilliams <petemcw@gmail.com>
2016-01-12 10:30:18 -0600

    Allows you to skip the APT auto-update by passing SKIP_UPDATE environment variable.
    Useful for one-off commands to speed up the process.

--- init/20_apt_update.sh
+++ init/20_apt_update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
-[ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
-[ "$APTLIST" ] && (echo "Now refreshing packages from apt repositories, this *may* take a while" && apt-get -yqq update && apt-get -yqq --only-upgrade install $APTLIST)
-sleep 1s
+if [ -z ${SKIP_UPDATE+x} ]; then
+    [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
+    [ "$APTLIST" ] && (echo "Now refreshing packages from apt repositories, this *may* take a while" && apt-get -yqq update && apt-get -yqq --only-upgrade install $APTLIST)
+    sleep 1s
+fi
