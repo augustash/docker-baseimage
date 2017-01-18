7cb1fc09bf301da01a7b04bb8f6907b0f73e34a2
Pete McWilliams <petemcw@gmail.com>
2017-01-18 14:53:51 -0600

    Removes recursive setting on volume permissions
    This was a performance bottleneck and ultimately unnecessary.

--- rootfs/etc/fix-attrs.d/10-user-dirs.sh
+++ rootfs/etc/fix-attrs.d/10-user-dirs.sh
@@ -1,4 +1,4 @@
-/backups true ash:ash 0644 0755
-/config true ash:ash 0644 0755
-/defaults true ash:ash 0644 0755
-/src true ash:ash 0644 0755
+/backups false ash:ash 0644 0755
+/config false ash:ash 0644 0755
+/defaults false ash:ash 0644 0755
+/src false ash:ash 0644 0755
